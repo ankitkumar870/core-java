221805add99954b6fdb414806532356cb6ff1cd9
ankitkumar870 <ankitsharma07488@gmail.com>
2026-07-03 21:46:17 +0530

    Add Personalinfo class to display user details

---
+++ 02-Variables/Personalinfo.java
@@ -0,0 +1,11 @@
+public class Personalinfo
+  {
+    public static void main(String[] args)
+    {
+      String name = "Ankit Kumar";
+      //String dob = "29/05/2004";
+      int age = 22;
+      System.out.println("Name: " + name);
+      System.out.println("Age: " + age);
+    }
+  }
